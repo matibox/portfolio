52ebb2cea8b5f1c1d91fc6bbf47c2c152a591f35
Matibox <mateusz.hladky@gmail.com>
2022-06-06 19:34:29 +0200

    Add scss variables, base styles and typography

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+// Styles
+import './styles/main.scss';
+
 // Animations
 import setAnimations from './animations';
 setAnimations();
 
-// Styles
-import './styles/main.scss';
-
 // Assets
+/*
 import Logo from './assets/Logo.svg';
 
 const logo = document.querySelector('[data-logo]');
 logo.src = Logo;
+*/
