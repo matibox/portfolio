febe73845508e124ddfae89116d059131ad2dd0f
Matibox <mateusz.hladky@gmail.com>
2022-06-07 19:07:40 +0200

    Add about section

--- src/assets.js
+++ src/assets.js
@@ -2,6 +2,7 @@ import Logo from './assets/Logo.svg';
 import Burger from './assets/Burger.svg';
 import BurgerClose from './assets/BurgerClose.svg';
 import Tilt from './assets/Tilt.svg';
+import AboutPhoto from './assets/AboutPhoto.png';
 
 export default function assets() {
     const assets = [
@@ -9,6 +10,7 @@ export default function assets() {
         { assetName: Burger, element: 'burger-img' },
         { assetName: BurgerClose, element: 'x-img' },
         { assetName: Tilt, element: 'tilt-transition' },
+        { assetName: AboutPhoto, element: 'about-photo' },
     ];
 
     function setAssetsSource(assets) {
