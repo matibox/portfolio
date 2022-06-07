450063bb2962da91491dfdbdfae6eadf396efc5c
Matibox <mateusz.hladky@gmail.com>
2022-06-07 18:45:25 +0200

    Finish hero

--- src/assets.js
+++ src/assets.js
@@ -1,12 +1,14 @@
 import Logo from './assets/Logo.svg';
 import Burger from './assets/Burger.svg';
 import BurgerClose from './assets/BurgerClose.svg';
+import Tilt from './assets/Tilt.svg';
 
 export default function assets() {
     const assets = [
         { assetName: Logo, element: 'logo' },
         { assetName: Burger, element: 'burger-img' },
         { assetName: BurgerClose, element: 'x-img' },
+        { assetName: Tilt, element: 'tilt-transition' },
     ];
 
     function setAssetsSource(assets) {
