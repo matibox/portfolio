d69caf7b679881678fe9fc71c5fc78ac269abd74
Matibox <mateusz.hladky@gmail.com>
2022-06-08 21:21:40 +0200

    Add sticky navbar Correct section paddings and margins

--- src/index.js
+++ src/index.js
@@ -18,11 +18,31 @@ assets();
 // Mobile menu
 const button = document.querySelector('[data-burger]');
 const menu = document.querySelector('[data-mobile-menu]');
+const navLinks = document.querySelectorAll('[data-nav-link]');
+const navbar = document.querySelector('[data-navbar]');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('header-top__mobile-menu--active');
+        button.classList.remove('header-top__burger--active');
+    });
+});
 
 button.addEventListener('click', () => {
     menu.classList.toggle('header-top__mobile-menu--active');
     button.classList.toggle('header-top__burger--active');
 });
+
+window.addEventListener('scroll', stickyNavbar);
+window.addEventListener('load', stickyNavbar);
+
+function stickyNavbar() {
+    if (window.scrollY > 0) {
+        navbar.classList.add('header-top--scroll');
+    } else {
+        navbar.classList.remove('header-top--scroll');
+    }
+}
 
 // Graphs
 const graphs = document.querySelectorAll('[data-graph]');
