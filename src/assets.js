61db41b7324ea6d9c36cd28c0a235bda9aced4d7
Matibox <mateusz.hladky@gmail.com>
2022-06-08 19:04:35 +0200

    Add basic projects section structure Change setAssetsSource() function, now it can process multiple img elements instead of a single element

--- src/assets.js
+++ src/assets.js
@@ -4,6 +4,7 @@ import BurgerClose from './assets/BurgerClose.svg';
 import Tilt from './assets/Tilt.svg';
 import AboutPhoto from './assets/AboutPhoto.png';
 import Wave from './assets/Wave.svg';
+import Github from './assets/Github.svg';
 
 export default function assets() {
     const assets = [
@@ -13,12 +14,15 @@ export default function assets() {
         { assetName: Tilt, element: 'tilt-transition' },
         { assetName: AboutPhoto, element: 'about-photo' },
         { assetName: Wave, element: 'wave-transition' },
+        { assetName: Github, element: 'github' },
     ];
 
     function setAssetsSource(assets) {
         assets.forEach(asset => {
-            const element = document.querySelector(`[data-${asset.element}]`);
-            element.src = asset.assetName;
+            const elements = document.querySelectorAll(
+                `[data-${asset.element}]`
+            );
+            elements.forEach(element => (element.src = asset.assetName));
         });
     }
 
